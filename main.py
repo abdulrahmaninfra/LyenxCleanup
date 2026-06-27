682d7ffa5ff70f9a6d34e14678c968cc369ef9a2
abdulrahmaninfra <abdelrahmaninfra@proton.me>
2026-06-27 18:31:26 +0300

    Restart Explorer Problem Solved & and improve restart windows input process

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 GREEN = "\033[92m"
 RED = "\033[91m"
@@ -33,29 +34,30 @@ for cmd in commands:
         print(f"{RED}Error: {e}{RESET}")
 
 
-# print(f"{GREEN}Restarting Windows Explorer!{RESET}")
-# try:
-#     subprocess.run("taskkill /f /im explorer.exe /t", shell=True, check=False)
-#     subprocess.Popen("%windir%\\explorer.exe", shell=True)
-# except Exception as e:
-#     print(f"{RED}Error: {e}{RESET}")
+print(f"{GREEN}Restarting Windows Explorer!{RESET}")
+try:
+    subprocess.run("taskkill /f /im explorer.exe", shell=True, check=False)
+    time.sleep(2)
+    subprocess.Popen("%windir%\\explorer.exe", shell=True)
+except Exception as e:
+    print(f"{RED}Error: {e}{RESET}")
 
 
 print(
-    "\n Do you wanna restart?\n 1. Yes, Press (1,yes,y,yeah,yep)\n 2. Nope (2,nah,no,neh,n)"
+    f"\n Do you wanna restart Windows?\n    1. {GREEN}Yes{RESET}\n    2. {RED}No{RESET}"
 )
 userRestartInput = input(" >>> ").strip().lower()
 
-if userRestartInput.startswith("y") or userRestartInput == 1:
+if userRestartInput.startswith("y") or userRestartInput == "1":
     run_command("shutdown /r /t 5")
     print(f"{RED}System will be restarted in 5 Seconds!!{RESET}")
 
-elif userRestartInput.startswith("n") or userRestartInput == 2:
+elif userRestartInput.startswith("n") or userRestartInput == "2":
     print(f"{GREEN}Thank you!!{RESET}")
-    print("MADE BY: github.com/abdulrahmaninfra\n AKA: Lyenx")
+    print("MADE BY: github.com/abdulrahmaninfra\nAKA: Lyenx")
     input("Press any key to exit")
 
 else:
     print(f"Invalid input, Cancel restarting")
-    print("MADE BY: github.com/abdulrahmaninfra\n AKA: Lyenx")
+    print("MADE BY: github.com/abdulrahmaninfra\nAKA: Lyenx")
     input("Press any key to exit")
