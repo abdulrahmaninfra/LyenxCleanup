c763d2e88197fc6bef0fd7b06b0cdd12eed3d25f
abdulrahmaninfra <abdelrahmaninfra@proton.me>
2026-06-28 20:01:45 +0300

    Refactor Cleanup class to improve error handling and execution feedback

--- main_oop.py
+++ main_oop.py
@@ -7,13 +7,23 @@ class Cleanup:
     def __init__(self, name: str,command: str):
         self.name = name
         self.command = command
+        self.is_complate = False
+        self.error_msg = None
 
     def execute(self):
         try:
-            subprocess.run(self.command, shell=True, check=True)
-        except Exception as cleanup_execute_error:
-            return self.cleanup_execute_error
+            run = subprocess.run(self.command, shell=True, check=True,stdout=subprocess.DEVNULL , stderr=subprocess.PIPE)
 
+            if run.returncode == 0:
+                self.is_complate = True
+                return True
+            else:
+                self.error_msg = run.stderr
+                return False
+            
+        except Exception as e:
+            self.error_msg = str(e)
+            return False
 
 
 class WinCleaner:
